280fbfe876240f2e2c8fbc6208e94e471c73ddde
Fredrik Hansson <fredrik.hansson@dotnetmentor.se>
2021-12-01 08:48:40 +0100

    Day 1 C#

---
+++ day1/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = File.ReadAllText("input.txt")
+  .Split("\n")
+  .Select(int.Parse)
+  .ToArray();
+
+int Measurements(int[] numbers, int winSize) {
+    var increments = 0;
+    for (var i = 0; i < numbers.Length - winSize; i++) {
+        var a = numbers[i..(i + winSize)].Sum();
+        var b = numbers[(i + 1)..(i + 1 + winSize)].Sum();
+        if (a < b) increments++;
+    }
+    return increments;
+}
+
+Console.WriteLine(Measurements(input, 1));
+Console.WriteLine(Measurements(input, 3));
